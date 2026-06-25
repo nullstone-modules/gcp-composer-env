2f3bb9c06aaf24dcf47c1556976cb7d41b1f0346
Brad Sickles <brad.sickles@gmail.com>
2026-06-25 18:30:26 -0400

    enable apis

--- gcp.tf
+++ gcp.tf
@@ -3,7 +3,18 @@ data "google_project" "this" {}
 
 locals {
   region         = data.google_client_config.this.region
-  zone           = data.google_client_config.this.zone
   project_id     = data.google_client_config.this.project
   project_number = data.google_project.this.number
+}
+
+resource "google_project_service" "composer_api" {
+  service                    = "composer.googleapis.com"
+  disable_on_destroy         = false
+  disable_dependent_services = false
+}
+
+resource "google_project_service" "secretmanager_api" {
+  service                    = "secretmanager.googleapis.com"
+  disable_on_destroy         = false
+  disable_dependent_services = false
 }
